5a66cc29eb9cc9732b33d5da6d488fb2cd6b80e3
koKekkoh <raoluba@outlook.jp>
2021-10-20 05:19:16 +0900

    0.1.0.2 #3

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 import unittest
 
-from . import testcase01, testcase02
+from . import testcase01, testcase02, testcase03
 
 def suites():
     suites = unittest.TestSuite()
     suites.addTests(unittest.makeSuite(testcase01.testIndexEntries))
     suites.addTests(unittest.makeSuite(testcase02.testIndexEntries))
+    suites.addTests(unittest.makeSuite(testcase03.testIndexEntries))
     return suites
