23dc3dc2ab615eb20b3b117db69823f1e0c2f79b
koKekkoh <raoluba@outlook.jp>
2021-10-24 12:55:36 +0900

    0.2.4

--- src/__init__.py
+++ src/__init__.py
@@ -10,7 +10,7 @@ A Sphinx Indexer.
 __copyright__ = 'Copyright (C) 2021 @koKekkoh'
 __license__ = 'BSD 2-Clause License'
 __author__  = '@koKekkoh'
-__version__ = '0.2.4b5' # 2021-10-24
+__version__ = '0.2.4' # 2021-10-24
 __url__     = 'https://github.com/KaKkouo/sphindexer'
 
 import re
