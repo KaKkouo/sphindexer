22208c7318d37f031dd126385954552027eb813d
koKekkoh <raoluba@outlook.jp>
2021-10-30 04:25:14 +0900

    coverage of XRefIndex.

--- tests/testcase61.py
+++ tests/testcase61.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import unittest
+from unittest.mock import Mock, MagicMock
+from docutils import nodes
 from docutils.parsers.rst.states import Inliner
+from sphinx.testing.util import assert_node
 from src import XRefIndex
 
 #-------------------------------------------------------------------
@@ -19,6 +22,26 @@ class testXRefIndex(unittest.TestCase):
         xref = XRefIndex()
         with self.assertRaises(AttributeError):
             rslt = xref('index', 'sphinx', 'sphinx', 1, inliner, {}, [])
+
+    def test03_TypeError(self):
+        text = 'sphinx'
+        xref = XRefIndex()
+        with self.assertRaises(TypeError):
+            rslt, msg = xref('index', text, text, 0, Mock())
+
+    def test04_TypeError(self):
+        text = 'sphinx<python>'
+        xref = XRefIndex()
+        with self.assertRaises(TypeError):
+            rslt, msg = xref('index', text, text, 0, Mock())
+
+    def test05_TypeError(self):
+        text = 'sphinx'
+        mock1 = MagicMock(return_value=(1, "msg"))
+        mock2 = MagicMock(return_value=(1, "msg"))
+        xref = XRefIndex()
+        with self.assertRaises(ValueError):
+            rslt, msg = xref('index', text, text, 0, mock1, mock2)
 
 #-------------------------------------------------------------------
 
