8be949c1603e1c138c6410a358d9d58a43050b40
koKekkoh <raoluba@outlook.jp>
2021-11-02 03:42:30 +0900

    coverage: \N{RIGHT-TO-LEFT MARK}

--- tests/testcase14.py
+++ tests/testcase14.py
@@ -11,32 +11,25 @@ testcase01i = {
     'doc3': [('single', 'Sphinx', 'id-131', '', None),
              ('single', 'Ель', 'id-132', '', None), ], 
     'doc4': [('single', 'ёлка', 'id-141', '', None),
-             ('single', '<200f>עברית<200e>', 'id-142', '', None), ], 
+             ('single', "‏עברית‎", 'id-142', '', None), ], 
     'doc5': [('single', '9-symbol', 'id-151', '', None),
              ('single', '&-symbol', 'id-152', '', None), ], 
     'doc6': [('single', '£100', 'id-161', '', None), ], 
 }
 
-testcase01o = [
-('Symbols',
-  [('&-symbol', [[('', 'doc5.html#id-152')], [], None]),
-   ('9-symbol', [[('', 'doc5.html#id-151')], [], None]),
-   ('<200f>עברית<200e>', [[('', 'doc4.html#id-142')], [], None]),
-   ('£100', [[('', 'doc6.html#id-161')], [], None])]),
- ('D',
-  [('docutils', [[('', 'doc1.html#id-111')], [], None])]),
- ('P',
-  [('pip',
-    [[],
-     [('install', [('', 'doc2.html#id-121')]),
-      ('upgrade', [('', 'doc2.html#id-122')])],
-     None]),
-   ('Python', [[('', 'doc1.html#id-112')], [], None])]),
- ('S', [('Sphinx', [[('', 'doc3.html#id-131')], [], None])]),
- ('Е',
-  [('ёлка', [[('', 'doc4.html#id-141')], [], None]),
-   ('Ель', [[('', 'doc3.html#id-132')], [], None])])
-]
+testcase01o0 = ('Symbols', [('&-symbol', [[('', 'doc5.html#id-152')], [], None]),
+                            ('9-symbol', [[('', 'doc5.html#id-151')], [], None]),
+                            ('£100', [[('', 'doc6.html#id-161')], [], None])])
+testcase01o1 = ('D', [('docutils', [[('', 'doc1.html#id-111')], [], None])])
+testcase01o2 = ('P', [('pip', [[],
+                      [('install', [('', 'doc2.html#id-121')]),
+                       ('upgrade', [('', 'doc2.html#id-122')])],
+                      None]),
+                      ('Python', [[('', 'doc1.html#id-112')], [], None])])
+testcase01o3 = ('S', [('Sphinx', [[('', 'doc3.html#id-131')], [], None])])
+testcase01o4 = ('Е', [('ёлка', [[('', 'doc4.html#id-141')], [], None]),
+                      ('Ель', [[('', 'doc3.html#id-132')], [], None])])
+testcase01o5 = ('ע', [('‏עברית‎', [[('', 'doc4.html#id-142')], [], None])])
 
 
 #-------------------------------------------------------------------
@@ -47,7 +40,13 @@ class testIndexEntries(unittest.TestCase):
         env = util.env(testcase01i)
         bld = util.builder(env)
         gidx = util.IndexEntries(env).create_index(bld)
-        self.assertEqual(testcase01o, gidx)
+        self.assertEqual(len(gidx), 6)
+        self.assertEqual(gidx[0], testcase01o0)
+        self.assertEqual(gidx[1], testcase01o1)
+        self.assertEqual(gidx[2], testcase01o2)
+        self.assertEqual(gidx[3], testcase01o3)
+        self.assertEqual(gidx[4], testcase01o4)
+        self.assertEqual(gidx[5], testcase01o5)
 
 #-------------------------------------------------------------------
 
