1a9f9240c82bfbbaf05b1e544a915b0d9c9e58a3
koKekkoh <raoluba@outlook.jp>
2022-02-24 06:02:23 +0900

    0.12.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 import sys
 from setuptools import setup
 
-import src as si
-
 setup(
-    version = si.__version__,
-    license = si.__license__,
-    author = si.__author__,
-    url = si.__url__,
     download_url = 'https://pypi.org/project/sphindexer/',
     project_urls = {
         'Code': 'https://github.com/KaKkouo/sphindexer',
